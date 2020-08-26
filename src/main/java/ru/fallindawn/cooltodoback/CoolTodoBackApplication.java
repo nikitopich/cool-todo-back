a9d5d82479d9148fe40ccdaeaf1ce32ee96e1ce3
nikitopich <babchenko.n1908@gmail.com>
2020-08-27 02:07:06 +0300

    Initial Commit

---
+++ src/main/java/ru/fallindawn/cooltodoback/CoolTodoBackApplication.java
@@ -0,0 +1,13 @@
+package ru.fallindawn.cooltodoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolTodoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolTodoBackApplication.class, args);
+	}
+
+}
