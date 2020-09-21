1f9fc2511de9114befce8590a4e6b7aefe79cb5d
nikitopich <babchenko.n1908@gmail.com>
2020-09-21 23:17:25 +0300

    Add some task to DB

--- src/main/resources/scipts/insert_users.sql
+++ src/main/resources/scipts/insert_users.sql
@@ -7,3 +7,7 @@ VALUES (nextval('sq_users_id'), 'admin', '$2a$04$l6jf/IelD8EcKEx0z5LJFur01DtdBcT
 
 INSERT INTO users_role
 VALUES (1,1), (2,2);
+
+INSERT INTO TASKS (id, deadline, name, users_id, ready)
+VALUES (nextval('sq_tasks_id'), '2020-01-10 00:00:00', 'admin most-important task-1', 1, false),
+       (nextval('sq_tasks_id'), '2021-01-01 00:00:00', 'admin most-important task-2', 1, false);
